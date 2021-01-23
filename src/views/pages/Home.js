2304dabb51ad63b963fe3ac4a41ad80385a08fa3
Raul Castro <raulc27@gmail.com>
2021-01-23 15:19:30 -0300

    ...

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,9 +1,15 @@
 let Home = {
     render: async () => {
         let view = `
-            <div>
-                <h1>View de nossa home</h1>
+        
+        <div class="row justify-content-center mt-5">
+            <div class="col-4 ">
+                <img src="https://pbs.twimg.com/profile_banners/32018803/1502307622/1500x500" class="img rounded img-responsive" width=100% />
             </div>
+            <div class="col-4">
+            <p class="lead text-justify">Olá, essa é a nossa plataforma, somos especialistas em resolver problemas que você não tinha antes de nos conhecer.</p>
+            </div>
+        </div>
         `
 
         return view
--- src/views/pages/SignUp.js
+++ src/views/pages/SignUp.js
@@ -1,5 +1,28 @@
+import baseURL from '../../service/baseURL.js';
+
+let postRegisterNewUser = async () => {
+    const options = {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    };
+
+    try{
+        const RegisterData = {
+            name: document.getElementById('username').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        }
+        const response = await fetch(baseURL,RegisterData, options)
+    }catch(err){
+        console.log('Ocorreu um erro',err)
+    }
+}
+
 let SignUp = {
     render: async () => {
+
         let view = `
             <div>
                 <h1>View de nossa SignUp</h1>
