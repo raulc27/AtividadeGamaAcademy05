c6f9b9ee1a82ea41f28d54f266c538b980b689ec
Raul Castro <raulc27@gmail.com>
2021-01-23 17:01:00 -0300

    ultimas alteracoes

--- src/views/pages/Login.js
+++ src/views/pages/Login.js
@@ -1,9 +1,29 @@
 let Login = {
     render: async () => {
         let view = `
-            <div>
-                <h1>View de nossa Login</h1>
+        <div class="row justify-content-center mt-5">
+        <div class="col-5 ">
+            <img src="https://pbs.twimg.com/profile_banners/32018803/1502307622/1500x500" class="img rounded img-responsive" width=100% height=300px />
+      
+            <p class="lead text-justify">Olá, essa é a nossa plataforma, somos especialistas em resolver problemas que você não tinha antes de nos conhecer.</p>
+      
             </div>
+      
+
+        <div class="col-5 ">
+        
+
+        <p class="lead mt-2">Faça o seu login</p>
+
+        <form class="mt-5 ">
+        
+        <input type="text" id="username" placeholder="USERNAME" class="form-control mt-3"/>
+        <input type="password" id="password" placeholder="password" class="form-control mt-1" />
+
+        <button class="btn btn-outline-dark form-control mt-5" type-"button"><b>Fazer login</b></button>
+        </for>
+    
+    </div>
         `
 
         return view
--- src/views/pages/SignUp.js
+++ src/views/pages/SignUp.js
@@ -24,15 +24,29 @@ let SignUp = {
     render: async () => {
 
         let view = `
-            <div class="col">
-                <div class="md-6">
-                <input type="text" id="username" placeholder="USERNAME" />
-                <input type="email" id="email" placeholder="EMAIL" />
-                <input type="password" id="password" placeholder="password" />
-
-                <button type="btn btn-outline-info"><b>Cadastrar</b></button>
-           
+        <div class="row justify-content-center mt-5">
+        <div class="col-5 ">
+            <img src="https://pbs.twimg.com/profile_banners/32018803/1502307622/1500x500" class="img rounded img-responsive" width=100% height=300px />
+      
+            <p class="lead text-justify">Olá, essa é a nossa plataforma, somos especialistas em resolver problemas que você não tinha antes de nos conhecer.</p>
+      
             </div>
+      
+
+        <div class="col-5 bg-secondary">
+        
+
+        <p class="lead mt-2">Cadastre-se</p>
+
+        <form class="mt-5 ">
+        <input type="text" id="username" placeholder="USERNAME" class="form-control mt-1" />
+        <input type="email" id="email" placeholder="EMAIL" class="form-control mt-1"/>
+        <input type="password" id="password" placeholder="password" class="form-control mt-1" />
+
+        <button class="btn btn-outline-light form-control mt-5" type-"button"><b>Cadastrar</b></button>
+        </for>
+    
+    </div>
         `
 
         return view
