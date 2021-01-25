f974a8fa61218dd249dcc0a284b63307809c2c01
Raul Castro <raulc27@gmail.com>
2021-01-25 20:57:17 -0300

    ultimas alteracoes, mudando url api, consertando signup

--- src/views/pages/SignUp.js
+++ src/views/pages/SignUp.js
@@ -5,11 +5,14 @@ document.postRegisterNewUser = async () => {
         method:'POST',
         headers:{
             'Content-Type':'application/json'
-        }
+        },
+        body: JSON.stringfy(RegisterData)
+        
     };
 
     try{
         const RegisterData = {
+            cpf: document.getElementById('cpf').value,
             name: document.getElementById('username').value,
             email: document.getElementById('email').value,
             password: document.getElementById('password').value
@@ -39,11 +42,13 @@ let SignUp = {
         <p class="lead mt-2">Cadastre-se</p>
 
         <form class="mt-5 ">
+        <input type="text" id="cpf" placeholder="C.P.F" class="form-control cpf-mask mt-1" />
         <input type="text" id="username" placeholder="USERNAME" class="form-control mt-1" />
+        <input type="text" id="name" placeholder="o seu nome" class="form-control mt-1" />
         <input type="email" id="email" placeholder="EMAIL" class="form-control mt-1"/>
         <input type="password" id="password" placeholder="password" class="form-control mt-1" />
 
-        <button class="btn btn-outline-light form-control mt-5" type-"button"><b>Cadastrar</b></button>
+        <button class="btn btn-outline-light form-control mt-5" id="submit_new_register" type-"button"><b>Cadastrar</b></button>
         </for>
     
     </div>
@@ -52,18 +57,47 @@ let SignUp = {
         return view
     },
     after_render: async ()=>{
-        document.getElementById('submit_new_register').addEventListener('click'), () => {
+        document.getElementById('submit_new_register').addEventListener('click', () => {
+          let  cpfVal = document.getElementById('cpf').value
           let  nameVal= document.getElementById('username').value
           let  emailVal= document.getElementById('email').value
           let  passwordVal= document.getElementById('password').value
-        
-          if (passwordVal.length > 6){
-              console.log('Mais de 6 caracteres na senha')
-          }else{
-              alert('confira sua senha!')
-          }
+          CPF = document.getElementById('cpf').value.replace(/[^\d]/g, "")
+          console.log(CPF)
 
-        }
+        
+
+
+
+          if(nameVal!='' && emailVal!='' && passwordVal!=''){
+
+            
+              axios.post(`${baseURL}usuarios`,{
+
+                cpf:CPF,
+                login:username,
+                passowrd:passwordVal,
+                name:nameVal
+
+              },{
+            
+                    headers:{
+                        'Content-Type':'applications/json'
+                    }
+            
+            })
+
+            .then( res=>{ 
+                if( res.status === 200){
+                    window.location.replace('#/login')
+                }
+            })
+
+            }else{
+                alert('confira sua senha!')
+            }
+
+        })
     }
 }
 
