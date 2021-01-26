96fdd2b8b4b1ef24dc5371cb31ec5c77129b669a
Raul Castro <raulc27@gmail.com>
2021-01-25 22:16:45 -0300

    ultimas alteracoes

--- src/views/pages/Dash.js
+++ src/views/pages/Dash.js
@@ -1,17 +1,24 @@
-
+import isAuthenticated from '../../service/isAuth.js';
 
 let Dash = {
     render: async () => {
+
+        let userData = JSON.parse(localStorage.getItem('userDataAccount'));
+        const {usuario,} = userData 
+        let IsAuth = await isAuthenticated(!localStorage.getItem('@toekn'),'login');
+
         let view = `
             <div>
                 <h1>View de nossa DASH</h1>
+                <h3>Olá, ${usuario.nome}</h3>
+                <p>${conta.saldo.toLocaleString('pt-br',{style:'currency', currency: 'BRL'})}</p>
             </div>
         `
 
         return view
     },
     after_render: async ()=>{
-
+        console.log()
     }
 }
 
